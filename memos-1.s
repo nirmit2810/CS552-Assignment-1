d50fea310f91e8620206b1bde1b1d69775a1dc39
timothychong <timothychong@outlook.com>
2015-02-22 10:41:47 +0800

    Attempted to make print hello world but not succeeding. Will proceed to trying to get memory
    important registers:
    as, si
    
    in real mode, memory is addressed by [as:si],
    look it up,
    and supposedly, si should point to msg,
    and lodsb should load a byte inside msg into register al,
    which is then printed onto the screen.
    For some reason it's not working.

--- memos-1.s
+++ memos-1.s
@@ -1,165 +1,37 @@
-
-# Test vga settings using real mode
-# This dumps various register values used for setting up a video
-# mode such as 16 color 640x480		
- 
 	.globl _start
-	
 	.code16
-
 _start:
-	movw $0x9000, %ax
-	movw %ax, %ss
-	xorw %sp, %sp
+	# setting up segment
+	movw $0x07C0, %ax
+	movw %ax, %ds
+	cld
 
-# set video mode	
-	movw $0x0003, %ax
+	# start to print string
+	movw msg, %si
+#	movl msg, %esi
+char_loop:
+	lodsb
+#	testb %al, %al
+#	jz done
+	# interrupting for output
+
+	movb $0x0E, %ah
 	int $0x10
+#	jmp char_loop
 
-#sequencer	
-	movw $0x3c4, %dx
-	xorb %al, %al
-	movw $5, %cx
-1:	
-	outb %al, %dx
-	incw %dx
-	pushw %ax
-	inb %dx, %al
-	decw %dx
+done:
 
-	call print
-	
-	popw %ax
-	incb %al
-	loop 1b
+msg: 
+	.asciz "Welcome"
+	len = . - msg			# length
 
-	movw $0x0e0d, %ax
-	movw $0x07, %bx
-	int $0x10
-	movw $0x0e0a, %ax
-	movw $0x07, %bx
-	int $0x10
-
-#attribute controller	
-	movw $0x3c0, %dx
-	movb $0x10, %al
-	movw $4, %cx
-1:	
-	outb %al, %dx
-	incw %dx
-	pushw %ax
-	inb %dx, %al
-	decw %dx
-	
-	call print
-	
-	popw %ax
-	incb %al
-	loop 1b
-
-	movb $0x34, %al
-	outb %al, %dx
-	incw %dx
-	inb %dx, %al
-
-	call print
-
-	movw $0x0e0d, %ax
-	movw $0x07, %bx
-	int $0x10
-	movw $0x0e0a, %ax
-	movw $0x07, %bx
-	int $0x10
-
-#graphics register
-	movw $0x3ce, %dx
-	xorb %al, %al
-	movw $9, %cx
-1:	
-	outb %al, %dx
-	incw %dx
-	pushw %ax
-	inb %dx, %al
-	decw %dx
-
-	call print
-	
-	popw %ax
-	incb %al
-	loop 1b
-
-	movw $0x0e0d, %ax
-	movw $0x07, %bx
-	int $0x10
-	movw $0x0e0a, %ax
-	movw $0x07, %bx
-	int $0x10
-
-#crt controller	
-	movw $0x3d4, %dx
-	xorb %al, %al
-	movw $25, %cx
-1:	
-	outb %al, %dx
-	incw %dx
-	pushw %ax
-	inb %dx, %al
-	decw %dx
-
-	call print
-	
-	popw %ax
-	incb %al
-	loop 1b
-
-	movw $0x0e0d, %ax
-	movw $0x07, %bx
-	int $0x10
-	movw $0x0e0a, %ax
-	movw $0x07, %bx
-	int $0x10
-
-#misc o/p register
-	movw $0x3cc, %dx
-	inb %dx, %al
-
-	call print
-
-1:	jmp 1b
-	
-print:	pushw %dx
-	movb %al, %dl
-	shrb $4, %al
-	cmpb $10, %al
-	jge 1f
-	addb $0x30, %al
-	jmp 2f
-1:	addb $55, %al		
-2:	movb $0x0E, %ah
-	movw $0x07, %bx
-	int $0x10
-
-	movb %dl, %al
-	andb $0x0f, %al
-	cmpb $10, %al
-	jge 1f
-	addb $0x30, %al
-	jmp 2f
-1:	addb $55, %al		
-2:	movb $0x0E, %ah
-	movw $0x07, %bx
-	int $0x10
-	popw %dx
-	ret
-
-# This is going to be in our MBR for Bochs, so we need a valid signature
 	.org 0x1FE
 
 	.byte 0x55
 	.byte 0xAA
 
 # To test:	
-# as --32 vga16.s -o vga16.o
-# ld -T vga.ld vga16.o -o vga16
-# dd bs=1 if=vga16 of=vga16_test skip=4096 count=512
-	
+# as --32 memos-1.s -o memos-1.o
+# ld -T memos-1.ld memos-1.o -o memos1
+# dd bs=1 if=memos1 of=memos1_test skip=4096 count=512
+# qemu-system-i386 memos1_test
