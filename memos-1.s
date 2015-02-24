09b2ba57b4335119241ce571141b74cff18ff0de
timothychong <timothychong@outlook.com>
2015-02-24 17:05:40 +0800

    Part 1 finished

--- memos-1.s
+++ memos-1.s
@@ -1,37 +1,79 @@
 	.globl _start
 	.code16
 _start:
+
 	# setting up segment
-	movw $0x07C0, %ax
+	movw $0, %ax
 	movw %ax, %ds
-	cld
 
 	# start to print string
-	movw msg, %si
-#	movl msg, %esi
+	leaw msg, %si
 char_loop:
 	lodsb
-#	testb %al, %al
-#	jz done
+ testb %al, %al
+ jz done_beginning
 	# interrupting for output
-
 	movb $0x0E, %ah
 	int $0x10
-#	jmp char_loop
+	jmp char_loop
+done_beginning:
 
-done:
+# Memory starts here
+	 movw $0xE801, %ax
+	 int $0x15
+	 addw $1024, %ax
+	 movw %ax ,%cx
+	 shrw $8, %ax
+	 call print
+	 movw %cx ,%ax
+	 call print
+	 jmp final
+
+print:	pushw %dx
+	movb %al, %dl
+	shrb $4, %al
+	cmpb $10, %al
+	jge 1f
+	addb $0x30, %al
+	jmp 2f
+1:	addb $55, %al		
+2:	movb $0x0E, %ah
+	movw $0x07, %bx
+	int $0x10
+
+	movb %dl, %al
+	andb $0x0f, %al
+	cmpb $10, %al
+	jge 1f
+	addb $0x30, %al
+	jmp 2f
+1:	addb $55, %al		
+2:	movb $0x0E, %ah
+	movw $0x07, %bx
+	int $0x10
+	popw %dx
+	ret
+
+final:
+	# start to print string
+	leaw msg_finish, %si
+char_loop_finish:
+	lodsb
+ testb %al, %al
+ jz done_beginning_finish
+	# interrupting for output
+	movb $0x0E, %ah
+	int $0x10
+	jmp char_loop_finish
+done_beginning_finish:
 
 msg: 
-	.asciz "Welcome"
-	len = . - msg			# length
+	.asciz "MemOS: Welcome *** System Memory is: 0x"
+msg_finish:
+	.asciz " Bytes.
+
 
 	.org 0x1FE
 
 	.byte 0x55
 	.byte 0xAA
-
-# To test:	
-# as --32 memos-1.s -o memos-1.o
-# ld -T memos-1.ld memos-1.o -o memos1
-# dd bs=1 if=memos1 of=memos1_test skip=4096 count=512
-# qemu-system-i386 memos1_test
