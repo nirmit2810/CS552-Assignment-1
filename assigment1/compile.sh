8584e31cdd24809c0aac08e191184e48ccd23d34
timothychong <timothychong@outlook.com>
2015-04-05 18:10:30 +0800

    Commmiting before starting modifications

--- assigment1/compile.sh
+++ assigment1/compile.sh
@@ -1,8 +1,5 @@
-mkdir image/
 gcc -std=gnu99 -c osdev.c -o osdev.o -fno-stack-protector
-mount -t ext2 disk.img image/ -o loop,offset=32256
+gcc -std=gnu99 -c output.c -o output.o -fno-stack-protector
 as --32 memos-2.s -o memos-2.o
-ld -T memos-2.ld memos-2.o osdev.o -o ./image/boot/memos-2
-umount image/
-rmdir image/
-qemu-system-i386 -m 256 disk.img
+ld -T memos-2.ld memos-2.o osdev.o output.o -o memos-2
+qemu-system-i386 -kernel memos-2
--- assigment1/memos-2.s
+++ assigment1/memos-2.s
@@ -1,29 +1,63 @@
-.section .bootstrap_stack, "aw", @nobits
-stack_bottom:
-.skip 16384 # 16 KiB
-stack_top:
+/*
+ * boot.S for memos-2
+ *
+ */
+	.global stack
+	
+	.bss
+	.align 0x1000
+	.comm stack, 0x1000 /* setup 4KB stack area in bss */
 
-.section .text
-.global _start
+.data
+	.align 0x4
+
+gdt:
+	.long 0
+	.long 0
+	//Code segment
+	.long 0x0000FFFF
+	.long 0x00CF9A00
+	//Kernel segment
+	.long 0x0000FFFF
+	.long 0x00CF9200
+
+gdt_ptr:
+	.short 0x7FF
+	.long gdt
+	
+.text
+	.globl _start
+
 _start:
-        jmp real_start
+	jmp real_start
 
-        /* Multiboot header -- Safe to place this header in 1st page of memory for GRUB */
-        .align 4
-        .long 0x1BADB002 /* Multiboot magic number */
-        .long 0x00000003 /* Align modules to 4KB, req. mem size */
-                         /* See 'info multiboot' for further info */
-        .long 0xE4524FFB /* Checksum */
+	/* Multiboot header -- Safe to place this header in 1st page for GRUB */
+	.align 4
+	.long 0x1BADB002 /* Multiboot magic number */
+	.long 0x00000003 /* Align modules to 4KB, req. mem size */
+			 /* See 'info multiboot' for further info */
+	.long 0xE4524FFB /* Checksum */
 
-real_start: 
+real_start:
+	//lgdt gdt_ptr
+	//ljmp $0x8, $1f
+1:
+	//movw 0x10, %ax
+	//movw %ax, %ss
+	//movw %ax, %ds
+	//movw %ax, %es
+	//movw %ax, %fs
+	//movw %ax, %gs
+	
+	/* set up stack */
+	movl $stack+0x1000, %esp /* setup 4Kbyte stack */
+	
+	/* save multiboot parameter, for eventual call to C code */
+	call init /* start of C code */
 
-        # This is where the rest of your program goes
-				movl $stack_top, %esp
-				pushl %ebx
-				call kernel_main
-				cli
-				hlt
-.loop:
-			jmp .loop
-
-.size _real_start, . - _real_start
+	/* In case we return from the call, we want to suspend the processor */
+	
+	cli
+	hlt
+loop:
+	jmp loop
